c3158a3a3b1d6c2d8cadb0f9dff39c4a319dc2d8
Oleksii Popov <opopov.contractor@libertyglobal.com>
2021-11-23 11:45:34 +0100

    Added field of view.

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use super::{xy_idx, Player, Position, State, TileType};
+use super::{Map, Player, Position, State, TileType, Viewshed};
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
@@ -6,13 +6,16 @@ use std::cmp::{max, min};
 pub fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let map = ecs.fetch::<Map>();
 
-    for (_, pos) in (&mut players, &mut positions).join() {
-        let dest_idx = xy_idx(pos.x + dx, pos.y + dy);
-        if map[dest_idx] != TileType::Wall {
+    for (_, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let dest_idx = map.xy_idx(pos.x + dx, pos.y + dy);
+        if map.tiles[dest_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + dx));
             pos.y = min(49, max(0, pos.y + dy));
+
+            viewshed.dirty = true;
         }
     }
 }
